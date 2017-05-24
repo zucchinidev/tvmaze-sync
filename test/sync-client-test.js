e15f5ef24c23e8a94c0a58e888ef0da243b6781b
Andrea Zucchini <azga84@gmail.com>
2017-05-24 19:06:18 +0200

    sync client test

--- test/sync-client-test.js
+++ test/sync-client-test.js
@@ -1,10 +1,22 @@
 import { test } from 'babel-tap'
-import { syncClient } from '../lib'
+import { sync } from '../lib'
 import { SyncClient } from '../lib/SyncClient'
+import { Configuration } from '../lib/configuration'
+
+const constants = {
+  DB_CONNECTION_STRING: 'mongodb://localhost:27017',
+  DB_NAME: 'tvmaze_sync_test',
+  COLLECTION_NAME: 'sync'
+}
+
+const configuration = new Configuration(constants)
 
 test('should create a sync client', (t) => {
-  t.ok(syncClient, 'should exist')
-  t.equals(typeof syncClient, 'object', 'should be an object')
-  t.ok(syncClient instanceof SyncClient, 'should be instance of SyncClient')
+  const client = sync.createClient(configuration)
+  t.ok(sync, 'should exist')
+  t.equals(typeof sync, 'object', 'should be an object')
+  t.ok(client instanceof SyncClient, 'should be instance of SyncClient')
+  t.ok(client.configuration instanceof Configuration, 'should be instance of SyncClient')
+  t.equals(typeof client.client, 'object', 'should be an object')
   t.end()
 })
