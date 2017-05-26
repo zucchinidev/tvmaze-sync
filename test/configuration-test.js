26e50388fa11365426475f91558c2de79720e59b
Andrea Zucchini <azga84@gmail.com>
2017-05-26 17:40:56 +0200

    improve configuration test

--- test/configuration-test.js
+++ test/configuration-test.js
@@ -1,7 +1,19 @@
 import { test } from 'babel-tap'
 import { Configuration } from '../lib/configuration'
-import { Constants } from '../lib/constants'
-const configuration = new Configuration(Constants.getConstants())
+
+const constants = {
+  mongo: {
+    dbConnection: 'mongodb://localhost:27017',
+    dbName: 'tvmaze_sync_test',
+    collectionName: 'sync'
+  },
+  request: {
+    maxNumberOfPages: 100,
+    initialPage: 0
+  }
+}
+
+const configuration = new Configuration(constants)
 
 test('should create a configuration instance', (t) => {
   t.ok(configuration, 'should exist')
@@ -10,11 +22,68 @@ test('should create a configuration instance', (t) => {
 })
 
 test('should create a configuration instance with db data', (t) => {
-  let config = configuration.getConfig()
+  const config = configuration.getConfig()
   t.ok(config, 'should exist')
-  t.equals(typeof config, 'object', 'should exist')
-  t.ok(config.dbConnection, 'should exist')
-  t.ok(config.dbName, 'should exist')
-  t.ok(config.collectionName, 'should exist')
+  t.equals(typeof config, 'object', 'config should exist')
+  t.ok(config.mongo.dbConnection, 'dbConnection should exist')
+  t.ok(config.mongo.dbName, 'dbName should exist')
+  t.ok(config.mongo.collectionName, 'collectionName should exist')
+  t.end()
+})
+
+test('should create a configuration instance with request data', (t) => {
+  const config = configuration.getConfig()
+  t.ok(config, 'should exist')
+  t.equals(typeof config, 'object', 'config should exist')
+  t.ok(config.request.maxNumberOfPages, 'maxNumberOfPages should exist in request')
+  t.equals(typeof config.request.initialPage, 'number', 'initialPage should exist in request')
+  t.equals(config.request.maxNumberOfPages, 100, 'max number of pages should be 100')
+  t.equals(config.request.initialPage, 0, 'initial page should be 0')
+  t.end()
+})
+
+test('should throw error for invalid configuration instance', (t) => {
+  try {
+    new Configuration({
+      request: {
+        maxNumberOfPages: 100,
+        initialPage: 0
+      }
+    })
+  } catch (err) {
+    const msg = 'Configuration not valid, mongo must be defined'
+    t.ok(err, 'There must be an error')
+    t.equals(err.message, msg, 'mongo is mandatory')
+  }
+  t.end()
+})
+
+test('should throw error for invalid configuration instance', (t) => {
+  const configuration = new Configuration({
+    mongo: {
+      dbConnection: 'mongodb://localhost:27017',
+      dbName: 'tvmaze_sync_test',
+      collectionName: 'sync'
+    }
+  })
+  t.ok(configuration, 'should exists')
+  t.equals(configuration.getConfig().request.initialPage, 0, '0 is the default page')
+  t.equals(configuration.getConfig().request.maxNumberOfPages, 100, '100 is the default maximum number of pages')
+  t.end()
+})
+
+test('should throw error for invalid configuration instance', (t) => {
+  t.doesNotThrow(function () {
+    const configuration = new Configuration({
+      mongo: {
+        dbConnection: 'mongodb://localhost:27017',
+        dbName: 'tvmaze_sync_test',
+        collectionName: 'sync'
+      }
+    })
+    t.ok(configuration, 'should exists')
+    t.equals(configuration.getConfig().request.initialPage, 0, '0 is the default page')
+    t.equals(configuration.getConfig().request.maxNumberOfPages, 100, '100 is the default maximum number of pages')
+  });
   t.end()
 })
